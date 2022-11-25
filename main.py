9135f60c79da55e64ffa021f6ab719e5598480b7
MierzeJevsky <mikolaj.mierzej@gmail.com>
2022-11-25 17:11:15 +0100

    Addapting main to handle list of params

--- main.py
+++ main.py
@@ -2,16 +2,28 @@ from tests import Test, AppendTest, CreateTest, ReadTest, ReadRangeTest, UpdateT
 from databases import Database, ListDatabase, DictionaryDatabase, TreeDatabase
 from data import Person
 
+params_list: list[int] = [
+    20001,
+    30001,
+    40001,
+    50001,
+    60001,
+    70001,
+    80001,
+    90001,
+    100001
+]
 
-def run_test(test: Test, databases) -> None:
+
+def run_test(test: Test, databases, size=None) -> None:
     print(f'Running {str(test)} test.')
-    duration = test.run(f'out/{str(test)}.csv', databases)
+    duration = test.run(f'out/{str(test)}.csv', databases, size)
     print(f'Test took: {str(duration)} seconds')
 
 
-def run_tests():
+def run_tests(param_size=None):
     databases: list[Database] = [ListDatabase[Person]([]), DictionaryDatabase[Person]([]), TreeDatabase[Person]([])]
-    run_test(CreateTest(), databases)
+    run_test(CreateTest(), databases, param_size)
     run_test(ReadTest(), databases)
     run_test(ReadRangeTest(), databases)
     run_test(UpdateTest(), databases)
@@ -21,4 +33,5 @@ def run_tests():
 
 
 if __name__ == '__main__':
-    run_tests()
+    for param in params_list:
+        run_tests(param)
