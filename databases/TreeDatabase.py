1a6916474a857acfd8c41c42a99fbad990713086
Kamil Osak <osak.k98@gmail.com>
2022-12-04 17:16:58 +0100

    poprawki

--- databases/TreeDatabase.py
+++ databases/TreeDatabase.py
@@ -1,6 +1,7 @@
 from databases import Database
 from typing import TypeVar, Generic
 from classes import Node
+import sys
 
 T = TypeVar('T')
 
@@ -11,6 +12,7 @@ class TreeDatabase(Generic[T], Database[T]):
     """
 
     def __init__(self, values: list[tuple[int, T]]) -> None:
+        sys.setrecursionlimit(5000)
         self.items = None
         for value in values:
             if self.items is None:
@@ -20,16 +22,19 @@ class TreeDatabase(Generic[T], Database[T]):
 
     def check(self):
         if depth(self.items.left) - depth(self.items.right) > 1:
-            self.items = rotation_left(self.items)
-        elif depth(self.items.right) - depth(self.items.left) > 1:
             self.items = rotation_right(self.items)
+        elif depth(self.items.right) - depth(self.items.left) > 1:
+            self.items = rotation_left(self.items)
 
     def add(self, key: int, value: T, left=None, right=None) -> bool:
         """Class method that adds value to the structure. In case of the already existing key, it returns false."""
-        if len(self.items.search(key)) > 0:
-            return False
-        self.items.insert(key, value, left, right)
-        self.check()
+        if self.items is not None:
+            if len(self.items.search(key)) > 0:
+                return False
+            self.items.insert(key, value, left, right)
+            self.check()
+        else:
+            self.items = Node(key, value, left, right)
 
     def get(self, key: int) -> T:
         """Class method that returns value under the specific key."""
